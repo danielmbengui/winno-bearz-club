08d41a17d5597eae25e1443a1b1e9a05ff5be7d6
dandelapp <daniel.mbengui@gmail.com>
2022-06-27 17:56:15 +0200

    update for mobile/tablet resize game objects

--- public/games/script.js
+++ public/games/script.js
@@ -16,16 +16,50 @@ let gameSpeed = 5;
 let nbLife = 3;
 let gameOver = false;
 
+
+
 // Parametrer les evenements de la souris...
 // quand on clique sur un endroit, le rond rouge (nous) se déplace a cet endroit
-let canvasPosition = canvas.getBoundingClientRect();
 
+
+
+
+
+
+let device = 'desktop';
+let ratioDevice = 1;
+var isMobile = false; //initiate as false
+    // device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+        isMobile = true;
+        device = 'mobile';
+        canvas.width = 700;
+        canvas.height = 250;
+        ratioDevice = 2;
+    }
 console.log('screen width', screen.width, 'screen height', screen.height);
 console.log('canvas', canvas.width, 'canvas width', canvas.height,);
+console.log('device', device)
+
+let canvasPosition = canvas.getBoundingClientRect();
+const mouse = {
+    x: canvas.width/2,
+    y: canvas.height/2,
+    click: false,
+}
 
 window.onload = function(){
 
+    
 
+
+document.getElementById('startGame').addEventListener('click', function() {
+	console.log('Start game');
+    //openFullscreen();
+    animate();
+    //openFullscreen();
+});
 
 document.getElementById('fullScreen').addEventListener('click', function() {
 	console.log('click full screen');
@@ -42,11 +76,7 @@ function openFullscreen() {
     }
   }
 
-const mouse = {
-    x: canvas.width/2,
-    y: canvas.height/2,
-    click: false,
-}
+
 
 
 
@@ -92,12 +122,15 @@ screen.orientation.addEventListener('change', function() {
     if( screen.orientation.type === 'portrait-primary' || screen.orientation.type === 'portrait-secondary' ){
         //alert('NOTHING');
     }else{
+        if( isMobile ){
+            //openFullscreen();
+        }
         //alert('IS OKAY !');
         //openFullscreen();
     }
     
 });
-    let device = 'desktop';
+    
     /*
     let orientation = 'portrait';
 
@@ -107,13 +140,7 @@ screen.orientation.addEventListener('change', function() {
     */
     //screen.orientation = orientation;
     //screen.lockOrientation(orientation);
-    var isMobile = false; //initiate as false
-    // device detection
-    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
-        isMobile = true;
-        device = 'mobile';
-    }
+    
 
     
      
@@ -151,6 +178,7 @@ canvas.addEventListener('mouseup', (event) => {
 
 
 
+
 const playerLeft = new Image();
 playerLeft.src = 'pic-team.png';
 const playerRight = new Image();
@@ -165,7 +193,7 @@ class Player {
         this.life = nbLife;
         this.x = canvas.width;
         this.y = canvas.height/2;
-        this.radius = 40;
+        this.radius = 40 / ratioDevice;
         this.angle = 0;
 
         this.frame = 0;
@@ -220,6 +248,7 @@ class Player {
         
         
         
+        
 
         ctx.save();
         //ctx.translate(this.x, this.y);
@@ -236,7 +265,7 @@ class Player {
             
 
             ctx.drawImage(playerRight, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, 
-                this.spriteWidth, this.spriteHeight, this.x - 66, this.y - 68, this.spriteWidth * 2, this.spriteHeight * 2);
+                this.spriteWidth, this.spriteHeight, this.x - 66/ratioDevice, this.y - 68/ratioDevice, this.spriteWidth * 2 / ratioDevice,this.spriteHeight * 2 / ratioDevice);
 
             //console.log('nb life', this.life);
                
@@ -262,7 +291,7 @@ class Enemy {
     constructor(){
         this.x = canvas.width + 200;
         this.y = Math.random() * (canvas.height - 150) + 90;
-        this.radius = 50;
+        this.radius = 50/ratioDevice;
         this.speed = Math.random() * 2 + 2;
         this.frame = 0;
         this.frameX = 0;
@@ -289,7 +318,7 @@ class Enemy {
         handleLife();
         
         ctx.drawImage(imgEnemy, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, 
-           this.spriteWidth, this.spriteHeight, this.x - 66, this.y - 68, this.spriteWidth * 2, this.spriteHeight * 2);
+           this.spriteWidth, this.spriteHeight, this.x - 66/ratioDevice, this.y - 68/ratioDevice, this.spriteWidth * 2 / ratioDevice, this.spriteHeight * 2 / ratioDevice);
            
            
            
@@ -382,7 +411,7 @@ class Bee {
     constructor(){
         this.x = Math.random() * canvas.width;
         this.y = canvas.height + 100;
-        this.radius = 40;
+        this.radius = 40/ratioDevice;
         this.speed = Math.random() * 5 + 1;
         this.distance;
         this.counted = false;
@@ -436,7 +465,7 @@ class Bee {
 
         
         ctx.drawImage(beeImage, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, 
-            this.spriteWidth, this.spriteHeight, this.x - 60, this.y - 72, this.spriteWidth * 2, this.spriteHeight * 2);
+            this.spriteWidth, this.spriteHeight, this.x - 60/ratioDevice, this.y - 72/ratioDevice, this.spriteWidth * 2 / ratioDevice, this.spriteHeight * 2 / ratioDevice);
             
 
             
@@ -577,7 +606,7 @@ const BG = {
 
 function handleBackground(){
     BG.x1 -= gameSpeed;
-    if( BG.x1 < -BG.width){ BG.x1 = BG.width-3; }
+    if( BG.x1 < -BG.width){ BG.x1 = BG.width - 3 * ratioDevice ; }
 
     BG.x2 -= gameSpeed;
     if( BG.x2 < -BG.width){ BG.x1 = 0;BG.x2 = BG.width; }
@@ -602,7 +631,7 @@ function handleBackground(){
     
 
     ctx.drawImage(salmon, SALMON.frameX * SALMON.spriteWidth, SALMON.frameY * SALMON.spriteHeight, 
-        SALMON.spriteWidth, SALMON.spriteHeight, SALMON.width/2, SALMON.height/2, SALMON.spriteWidth/3, SALMON.spriteHeight/3);
+        SALMON.spriteWidth, SALMON.spriteHeight, SALMON.width/2, SALMON.height/2, isMobile ? SALMON.spriteWidth/6 : SALMON.spriteWidth/3, isMobile ? SALMON.spriteHeight/6 : SALMON.spriteHeight/3);
 
         //ctx.drawImage(level, HEART.x1, HEART.y, this.radius * 2.8, this.radius * 2.8);
     //ctx.drawImage(salmon, BG.x1, BG.y, background.width, BG.height);
@@ -662,12 +691,12 @@ function handleLife(){
     if( player.life >= 0 ){
         level.src = `sprite/life${player.life}.png`;
             //ctx.drawImage(level, HEART.x1 + 10, HEART.y + 10, HEART.spriteWidth/3, HEART.spriteHeight/3);
-        ctx.drawImage(level, HEART.x1 + 10, HEART.y + 10, HEART.spriteWidth/3, HEART.spriteHeight/3); 
+        ctx.drawImage(level, HEART.x1 + 10, HEART.y + 10, HEART.spriteWidth/3/ratioDevice, HEART.spriteHeight/3/ratioDevice); 
              
     }else{
         //player.life = 0;
         level.src = `sprite/life0.png`;
-        ctx.drawImage(level, HEART.x1 + 10, HEART.y + 10, HEART.spriteWidth/3, HEART.spriteHeight/3); 
+        ctx.drawImage(level, HEART.x1 + 10, HEART.y + 10, HEART.spriteWidth/3/ratioDevice, HEART.spriteHeight/3/ratioDevice); 
             //handleGameOver();   
             
             //ctx.fillStyle = 'red';
@@ -678,11 +707,11 @@ function handleLife(){
     } 
    
     
-    ctx.drawImage(imgScore, HEART.x1 + 20 + HEART.spriteWidth/3, HEART.y + 10, HEART.spriteHeight/3, HEART.spriteHeight/3);  
+    ctx.drawImage(imgScore, HEART.x1 + 20 + HEART.spriteWidth/3/ratioDevice, HEART.y + 10, HEART.spriteHeight/3/ratioDevice, HEART.spriteHeight/3/ratioDevice);  
     ctx.fillStyle = 'red';
     //ctx.font();
-    ctx.font = '60px VT323 bold';
-    ctx.fillText("Score : " + score, HEART.x1 + 30 + HEART.spriteWidth/3 + HEART.spriteHeight/3, HEART.spriteHeight/3);
+    ctx.font = isMobile ? '30px VT323 bold' : '60px VT323 bold';
+    ctx.fillText("Score : " + score, HEART.x1 + 30 + HEART.spriteWidth/3/ratioDevice + HEART.spriteHeight/3/ratioDevice, HEART.spriteHeight/3/ratioDevice);
     
 }
     // Animation loop
@@ -718,15 +747,59 @@ function animate(){
     
 }
 
-animate();
+
 //mySound.play();
 }
 
 window.addEventListener('resize', () => {
+    canvas.removeEventListener('mousedown',
+(event) => {
+    mouse.click = false;
+    mouse.x = event.x - canvasPosition.left;
+    mouse.y = event.y - canvasPosition.top;
+    console.log(mouse.x, mouse.y)
+},
+    false
+  );
+
+  canvas.removeEventListener('mouseup',
+  (event) => {
+      mouse.click = false;
+      mouse.x = event.x - canvasPosition.left;
+      mouse.y = event.y - canvasPosition.top;
+      console.log(mouse.x, mouse.y)
+  },
+      false
+    );
     console.log('resize screen')
     //canvas.width = screen.width * (53.5/100);
     //canvas.height = screen.height * (47.5/100);
+    canvas.width = 1024;
+        canvas.height = 512;
+        ratioDevice = 1;
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+        isMobile = true;
+        device = 'mobile';
+        canvas.width = 700;
+        canvas.height = 250;
+        ratioDevice = 2;
+    }
     canvasPosition = canvas.getBoundingClientRect();
+    canvas.addEventListener('mousedown', (event) => {
+        mouse.click = true;
+        mouse.x = event.x - canvasPosition.left;
+        mouse.y = event.y - canvasPosition.top;
+        console.log(mouse.x, mouse.y)
+    });
+    
+    canvas.addEventListener('mouseup', (event) => {
+        mouse.click = false;
+        mouse.x = event.x - canvasPosition.left;
+        mouse.y = event.y - canvasPosition.top;
+        console.log(mouse.x, mouse.y)
+    });
+    //handleBackground();
     //var clickEvent = document.createEvent('MouseEvents');
     //clickEvent.initEvent('mouseup', true, true);
     //canvas.click();
