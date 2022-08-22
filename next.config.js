18b7f515a2cb0283e57a7d419b7696ed70de61b9
dandelapp <daniel.mbengui@gmail.com>
2022-08-22 23:50:01 +0200

    add eslint on package.json success

--- next.config.js
+++ next.config.js
@@ -4,9 +4,14 @@
 const nextConfig = {
     reactStrictMode: false,
     webpack5: true,
-    webpack : (config) => {
-        config.resolve.fallback = { fs: false};
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
         return config;
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
     },
 }
 
