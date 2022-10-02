622f00481653f77ff86c24045a69cfd171cf170c
dandelapp <daniel.mbengui@gmail.com>
2022-10-02 02:09:19 +0200

    remove/hide api keys

--- pages/_app.js
+++ pages/_app.js
@@ -20,20 +20,6 @@ import { useTheme, } from '@mui/material/styles';
 require('dotenv').config({path: "./vars/.env"});
 
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD52vVRfDYZgLdp0x9ykT5RbAS55dC2j10",
-  authDomain: "winnobearznft.firebaseapp.com",
-  projectId: "winnobearznft",
-  storageBucket: "winnobearznft.appspot.com",
-  messagingSenderId: "462203420048",
-  appId: "1:462203420048:web:2421178264238d2e018715",
-  measurementId: "G-R0FZ8NJJ41"
-};
-
-
-
-/*
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -43,7 +29,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-*/
+
 const app = initializeApp(firebaseConfig);
 //const database = getFirestore(app);
 //const app = initializeApp(firebaseConfig);
